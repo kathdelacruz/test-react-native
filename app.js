5c3e4e143d49abd15ae17a3d76e1bf0c756aa041
Katherine <katherine.delacruz8@gmail.com>
2017-05-01 18:11:17 -0500

    Add reusable box

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -14,33 +8,26 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import ArtistBox from './ArtistBox';
 
 export default class testReactNative extends Component {
 
   render() {
-    const image = 'https://buzz.ie/wp-content/uploads/2016/12/chris-martin-israel.jpg'
-    const name = 'Chris Martin'
-    const likes = 200
-    const comments = 140
+    const artist = {
+      image: 'https://buzz.ie/wp-content/uploads/2016/12/chris-martin-israel.jpg',
+      name: 'Chris Martin',
+      likes: 200,
+      comments: 140
+    }
 
     return (
       <View style={styles.container}>
-        <View style={styles.artistBox}>
-          <Image style={styles.image} source={{ uri: image }} />
-          <View style={styles.info}>
-            <Text style={styles.name}>{name}</Text>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-heart-outline" size={30} color="gray" />
-                <Text style={styles.count}>{likes}</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Icon name="ios-chatboxes-outline" size={30} color="gray" />
-                <Text style={styles.count}>{comments}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
+        <ArtistBox artist={artist} />
+        <ArtistBox artist={artist} />
+        <ArtistBox artist={artist} />
+        <ArtistBox artist={artist} />
+        <ArtistBox artist={artist} />
+        <ArtistBox artist={artist} />
       </View>
     );
   }
@@ -52,38 +39,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     paddingTop: 50,
   },
-  artistBox: {
-    backgroundColor: 'white',
-    flexDirection: 'row'
-  },
-  image: {
-    height: 150,
-    width: 150,
-  },
-  info: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center'
-  },
-  name: {
-    fontSize: 20,
-    marginTop: 10,
-    color: '#333'
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 40,
-    marginTop: 15,
-  },
-  iconContainer: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  column: {
-    color: 'gray'
-  }
 });
 
 AppRegistry.registerComponent('testReactNative', () => testReactNative);
