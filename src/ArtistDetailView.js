c66f2f209b679a23f0b023962e8d4b6fecd77fd2
Katherine <katherine.delacruz8@gmail.com>
2017-05-10 22:36:39 -0500

    Add comments

--- src/ArtistDetailView.js
+++ src/ArtistDetailView.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import { firebaseDatabase, firebaseAuth } from './firebase';
 import ArtistBox from './ArtistBox';
 import { getArtists } from './api-client'
 
 export default class ArtistDetailView extends Component {
+
+  handleSend = () => {
+    const { text } = this.state;
+    const artistCommentsRef = this.getArtistCommentsRef();
+    var newCommentRef = artistCommentsRef.push();
+    newCommentRef.set({ text });
+  }
+
+  getArtistCommentsRef = () => {
+    const { id } = this.props.artist;
+    return firebaseDatabase.ref(`comments/${id}`)
+  }
+
+  handleChangeText = (text) => this.setState({text})
 
   render() {
     const artist = this.props.artist;
@@ -16,6 +33,16 @@ export default class ArtistDetailView extends Component {
     return (
       <View style={styles.container}>
         <ArtistBox artist={artist} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Opina sobre este artista"
+            onChangeText={this.handleChangeText}
+          />
+          <TouchableOpacity onPress={this.handleSend}>
+            <Icon name="ios-send-outline" size={30} color="#e74c4c" />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -26,5 +53,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'lightgray',
     paddingTop: 70,
+  },
+  inputContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    height: 50,
+    backgroundColor: 'white',
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    height: 50,
+    flex: 1,
   },
 });
